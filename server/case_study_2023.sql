c68c09a720c318e46f14232b1f4441025820b898
Stephen Aguilar <stephenonline25@gmail.com>
2023-10-01 05:03:31 +0800

    exported sql

--- server/case_study_2023.sql
+++ server/case_study_2023.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 29, 2023 at 05:33 PM
+-- Generation Time: Sep 30, 2023 at 11:03 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `admin` (
   `admin_id` int(11) NOT NULL,
   `admin_username` varchar(50) NOT NULL,
-  `admin_password` varchar(50) NOT NULL,
+  `admin_password` varchar(255) NOT NULL,
   `admin_email_address` varchar(45) NOT NULL,
   `admin_firstname` varchar(45) NOT NULL,
   `admin_surname` varchar(45) NOT NULL
@@ -41,10 +41,8 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`admin_id`, `admin_username`, `admin_password`, `admin_email_address`, `admin_firstname`, `admin_surname`) VALUES
-(1, 'stephenpogi', 'pbkdf2:sha256:600000$Cqr1WH2fG7BxYGDj$79a30e480056', 'stephenonline25@gmail.com', 'Stephen Joaquin', 'Aguilar'),
-(3, 'stephenpogi123', 'pbkdf2:sha256:600000$kSZJNIThYOTLmvDX$67156cfd836c', 'stephenonline25@gmail.com', 'Stephen Joaquin', 'Aguilar'),
-(4, 'stephenpogi1231', 'pbkdf2:sha256:600000$j5TF51wSiMzT9lwA$52c6eaca9281', 'stephenonline25@gmail.com', 'Stephen Joaquin', 'Aguilar'),
-(5, 'tipanochi', 'pbkdf2:sha256:600000$emsrlGizDJE4QwhX$ba66d5ee3c39', 'stephenonline25@gmail.com', 'Stephen Joaquin', 'Aguilar');
+(6, 'pogisipanot', 'pbkdf2:sha256:600000$v2INXFPBsE7r4pYB$40df1c4e042a6207d364b86a93be49b753c7a7bd5aa9a7d303956214e7fd7b61', 'hayahay', 'google', 'microsoft'),
+(7, 'admin', 'pbkdf2:sha256:600000$Lk24rQPircJ8FLEQ$2f8c282c39bb01544533a8a1468be535c9db3cb0a54e71ed1a2d4393dfbed9ce', 'admin@gmail.com', 'super', 'user');
 
 -- --------------------------------------------------------
 
@@ -72,9 +70,17 @@ INSERT INTO `alembic_version` (`version_num`) VALUES
 CREATE TABLE `borrowed` (
   `borrow_id` int(11) NOT NULL,
   `time_quota` time DEFAULT NULL,
-  `is_verified` tinyint(1) DEFAULT NULL,
+  `is_returned` tinyint(1) DEFAULT NULL,
   `pending_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `borrowed`
+--
+
+INSERT INTO `borrowed` (`borrow_id`, `time_quota`, `is_returned`, `pending_id`) VALUES
+(12, '08:43:24', 0, 7),
+(13, '08:43:24', 0, 8);
 
 -- --------------------------------------------------------
 
@@ -96,9 +102,9 @@ CREATE TABLE `equipment` (
 
 INSERT INTO `equipment` (`equip_id`, `equip_type`, `equip_unique_key`, `is_available`, `is_pending`) VALUES
 (2, 'basketball', 'anjishije', 0, 1),
-(3, 'badminton', 'nikjhgasdf', 0, 1),
+(3, 'badminton', 'nikjhgasdf', 1, 0),
 (4, 'volleyball', 'ghyuewoa', 0, 1),
-(5, 'tennis', 'oafneuqfjsa', 0, 1);
+(5, 'tennis', 'oafneuqfjsa', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -120,10 +126,8 @@ CREATE TABLE `pending` (
 --
 
 INSERT INTO `pending` (`pending_id`, `equip_type`, `equip_unique_key`, `student_number`, `student_name`, `is_verified`) VALUES
-(1, 'basketball', 'anjishije', 'AY2021-00752', 'Aguilar, Stephen', 0),
-(3, 'badminton', 'nikjhgasdf', 'AY2021-00753', 'Aguilar, Joaquin', 0),
-(5, 'volleyball', 'ghyuewoa', 'AY2021-00853', 'Aguilar, sample', 0),
-(6, 'tennis', 'oafneuqfjsa', 'AY-2021-00816', 'year, first', 0);
+(7, 'volleyball', 'ghyuewoa', 'AY-2021-00752', 'Aguilar, Stephen Joaquin', 1),
+(8, 'basketball', 'anjishije', 'sampl', 'sample, sample', 1);
 
 -- --------------------------------------------------------
 
@@ -149,10 +153,8 @@ CREATE TABLE `student` (
 --
 
 INSERT INTO `student` (`student_id`, `student_number`, `student_section`, `student_department`, `student_year`, `student_email_address`, `student_firstname`, `student_surname`, `requested_item`, `status`) VALUES
-(7, 'AY2021-00752', '3a3', 'IT', '3rd', 'stephenonline25@gmail.com', 'Stephen', 'Aguilar', 'anjishije', 'requested'),
-(8, 'AY2021-00753', '3a3', 'IT', '3rd', 'stephenonline25@gmail.com', 'Joaquin', 'Aguilar', 'nikjhgasdf', 'requested'),
-(9, 'AY2021-00853', '2a1', 'IT', '2nd', 'sample@gmail.com', 'sample', 'Aguilar', 'ghyuewoa', 'requested'),
-(10, 'AY-2021-00816', '1a4', 'IT', '1st', '1styear@gmail.com', 'first', 'year', 'oafneuqfjsa', 'requested');
+(11, 'AY-2021-00752', '3a3', 'IT', '3rd', 'stephenonline25@gmail.com', 'Stephen Joaquin', 'Aguilar', 'ghyuewoa', 'to-receive'),
+(12, 'sampl', 'sample', 'samp[le', 'sample', 'sample', 'sample', 'sample', 'anjishije', 'to-receive');
 
 --
 -- Indexes for dumped tables
@@ -207,13 +209,13 @@ ALTER TABLE `student`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `borrowed`
 --
 ALTER TABLE `borrowed`
-  MODIFY `borrow_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `borrow_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `equipment`
@@ -225,13 +227,13 @@ ALTER TABLE `equipment`
 -- AUTO_INCREMENT for table `pending`
 --
 ALTER TABLE `pending`
-  MODIFY `pending_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `pending_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Constraints for dumped tables
