57beabb334992f73242fcf6576b6a2e60e97deb7
Stephen Aguilar <stephenonline25@gmail.com>
2023-11-23 04:23:32 +0800

    updated changes prior to the live code

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,23 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Greeting from './greetings';
 import Dashboard from './dashboard';
 import RequestEquipment from './request-equipment';
-
+import Header from './header.js';
+import Footer from './footer.js';
 
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" exact element={<Greeting />} />
-        <Route path="/dashboard" exact element={<Dashboard />} />
-        <Route path="/request-equipment/:equip_unique_key" element={<RequestEquipment />} />
-      </Routes>
-    </Router>
+    <div>
+    <Header />
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Greeting />} />
+          <Route path="/dashboard" exact element={<Dashboard />} />
+          <Route path="/request-equipment/:equip_unique_key" element={<RequestEquipment />} />
+        </Routes>
+      </Router>
+    <Footer />
+    </div>
   );
 }
 
